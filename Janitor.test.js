0f3103c37b22223bacd2888257a8c4297af8eb6a
Aden802 <ab.aden33@gmail.com>
2019-09-22 00:05:06 -0400

    Janitor.js

---
+++ Janitor.test.js
@@ -0,0 +1,10 @@
+const Janitor = require("./Janitor");
+
+describe ("Janitor", () => {
+    const underTest = new Janitor();
+
+
+        expect(testJanitor.getSweeping()).toEqual(false);
+
+    })
+ 
